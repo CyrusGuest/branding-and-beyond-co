4a5b6c50585558ea48fcba2f314d58f917573cd7
Cyrus Guest <cyrus.t.guest@gmail.com>
2022-07-22 16:48:01 -0400

    Dark mode mobile nav updated

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,25 +1,22 @@
 import React, { useContext } from 'react'
 import logo from '../../images/logo.svg'
-import darkLogo from '../../images/dark_mode/logo.svg'
 import menuDots from '../../images/menu-dots.svg'
-import darkMenuDots from '../../images/dark_mode/menu-dots.svg'
 import arrow from '../../images/arrow.svg'
-import darkArrow from '../../images/dark_mode/arrow.svg'
 import { Link } from 'react-router-dom'
 import { MenuContext } from '../context/MenuContext'
 
 const MobileNav = () => {
-  let { MobileNavOpen, setMobileNavOpen, LightMode } = useContext(MenuContext)
+  let { MobileNavOpen, setMobileNavOpen } = useContext(MenuContext)
 
   return (
-    <div className='fixed z-20 w-3/4 h-full bg-white dark:bg-brand-primary right-0'>
+    <div className='fixed z-20 w-3/4 h-full bg-white dark:bg-brand-secondary right-0'>
       <div className='flex '>
-        {LightMode ? <img onClick={() => setMobileNavOpen(false)} src={arrow} alt="" className='cursor-pointer mr-8' /> : <img onClick={() => setMobileNavOpen(false)} src={darkArrow} alt="" className='cursor-pointer mr-8' />}
-        {LightMode ? <img src={logo} alt="" className='cursor-pointer mx-auto' /> : <img src={darkLogo} alt="" className='cursor-pointer mx-auto' />}
-        {LightMode ? <img onClick={() => setMobileNavOpen(!MobileNavOpen)} src={menuDots} alt="" className='cursor-pointer ml-auto mr-4' /> : <img onClick={() => setMobileNavOpen(!MobileNavOpen)} src={darkMenuDots} alt="" className='cursor-pointer ml-auto mr-4' />}
+        <img onClick={() => setMobileNavOpen(false)} src={arrow} alt="" className='cursor-pointer mr-8' />
+        <img src={logo} alt="" className='cursor-pointer mx-auto' />
+        <img onClick={() => setMobileNavOpen(!MobileNavOpen)} src={menuDots} alt="" className='cursor-pointer ml-auto mr-4' />
       </div>
 
-      <div className='flex flex-col ml-6 mt-10 text-brand-primary dark:text-white gap-10'>
+      <div className='flex flex-col ml-6 mt-10 text-brand-primary  gap-10'>
         <ul>
           <h1 className='font-bold text-5xl'>shop</h1>
           <div className='text-2xl'>
